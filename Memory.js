9f45d1e101e0cc50af199d36c319e09a7e87a090
Ken Shamrock L. Dizon <dizonshamrock@gmail.com>
2023-11-13 19:34:24 +0800

    Added module.exports = Memory

--- Memory.js
+++ Memory.js
@@ -10,7 +10,7 @@ class Memory {
         this.availableSize = this.totalMemorySize - this.osSize;
         
         // Representation of the physical RAM
-        this.wholeMemory = new Array();
+        this.wholeMemory = [];
     }
 
     initialAllocation() {
@@ -18,13 +18,19 @@ class Memory {
         let i = 0;
         while (i < this.queue.length) {
             // Determine if job can fit in memory
-            if (this.queue.length[i].jobSize < this.availableSize) {
+            if (this.queue[i].jobSize < this.availableSize) {
                 // push to memory if it can fit
-                this.wholeMemory.push(this.queue.length[i]);
+                this.wholeMemory.push(this.queue[i]);
 
                 // Dequeue job
-                this.queue = this.queue;
+                // this.waitingQueue = this.waitingQueue.slice(0, idx).concat(this.wholeMemory.slice(idx+1));
+                this.availableSize -= this.queue[i].jobSize;
+                this.queue = this.queue.slice(0, i).concat(this.queue.slice(i+1));
+            } else {
+                i++;
             }
         }
     }
 }
+
+module.exports = Memory;
