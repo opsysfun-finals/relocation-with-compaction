21dc5cbfa0bd089714b10fc4af3e996164a5dcac
Ken Shamrock L. Dizon <dizonshamrock@gmail.com>
2023-11-13 21:54:15 +0800

    Added values used to test algorithm.

--- Main.js
+++ Main.js
@@ -1,5 +1,19 @@
-class Main {
-    constructor() {
-        
-    }
+const Memory = require('./Memory.js');
+const Job = require("./Job.js");
+
+function main() {
+    const jobList = [new Job("job1", 100), new Job("job2", 200), new Job("job3", 300)];
+    ram = new Memory(500, 100, jobList);
+    ram.viewMemory();
+    ram.viewQueue();
+
+    console.log("\nDeallocation");
+    ram.deallocate([1]);
+    ram.viewMemory();
+    
+    console.log("\nCompaction");
+    ram.compaction();
+    ram.viewMemory();
 }
+
+main();
